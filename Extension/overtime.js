ec0514025fcbfce8f3a710d22f9038bfac02f459
Abhishek L. Deore <dr.abhi7deore@gmail.com>
2017-12-25 12:27:21 +0530

    Updated to make minor changes

--- Extension/overtime.js
+++ Extension/overtime.js
@@ -1,23 +1,36 @@
 function markDayStartTime(time) {
+	// will be replaced with storage 
 	console.log("Start Time : " + time);
 }
 
-function setAlarm(time){
-	chrome.alarms.create("EndWork", {when : time + 1000});
-}
-
 function saveEntry(time){
+	// will be replaced with storage
 	console.log("End Time : " + time);
 }
 
-function onClickMarkTimeButton(){
+function setAlarm(time){
+	chrome.alarms.create("EndWork", {when : time});
+}
+
+// Event Listeners
+function onClickMarkStartButton(){
+	document.getElementById("btnMarkStart").disabled = true;
+
 	var start = Date.now();
 	markDayStartTime(start);
-	setAlarm(start + 10);
+
+	var end = start + (9 * 60 * 60 * 1000);
+	setAlarm(end);
+}
+
+function onClickMarkEndButton(){
+	var end = Date.now();
 }
 
 chrome.alarms.onAlarm.addListener(function(alarm){
 		console.log("End time: " + Date.now());
+		alert('Day is over! Good Bye :)');
 });
 
-document.getElementById("btnMarkTime").addEventListener("click", onClickMarkTimeButton);
+document.getElementById("btnMarkStart").addEventListener("click", onClickMarkStartButton);
+document.getElementById("btnMarkEnd").addEventListener("click", onClickMarkEndButton);
