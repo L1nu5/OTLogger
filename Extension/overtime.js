3d208834b82f4f0b95c4e5b1636473558e00837a
L1nu5 <dr.abhi7deore@gmail.com>
2017-12-25 22:41:40 +0530

    Added a method to decide state of Start Time button

--- Extension/overtime.js
+++ Extension/overtime.js
@@ -1,8 +1,18 @@
+function onLoad(){
+	chrome.storage.sync.get("allowStartTimeBtn", function(obj){
+		if(obj.allowStartTimeBtn){
+			document.getElementById('btnMarkStart').disabled = false;
+		}else{
+			document.getElementById('btnMarkStart').disabled = true;
+		}
+	});
+}
+
 function markDayStartTime(time) {
 	var today = new Date();
 	var date = today.getDay() + "/" + today.getMonth() + "/" + today.getFullYear();
 
-	chrome.storage.sync.set({'day':date,'start':time},function(){
+	chrome.storage.sync.set({'day':date,'start':time}, function(){
 		console.log("Date: " + date + " Start Time: " + time + " Saved!");
 	});
 }
@@ -19,13 +29,15 @@ function setAlarm(time){
 
 // Event Listeners
 function onClickMarkStartButton(){
-	document.getElementById("btnMarkStart").disabled = true;
-
 	var start = Date.now();
 	markDayStartTime(start);
 
 	var end = start + (9 * 60 * 60 * 1000);
 	setAlarm(end);
+
+	chrome.storage.sync.set({'allowStartTimeBtn':false}, function(){
+		console.log("button value saved");
+	});
 }
 
 function onClickMarkEndButton(){
@@ -40,3 +52,5 @@ chrome.alarms.onAlarm.addListener(function(alarm){
 
 document.getElementById("btnMarkStart").addEventListener("click", onClickMarkStartButton);
 document.getElementById("btnMarkEnd").addEventListener("click", onClickMarkEndButton);
+
+document.addEventListener('DOMContentLoaded', onLoad, false);
