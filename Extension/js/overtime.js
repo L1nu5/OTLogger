ff9ebd2f5c4a333dd904a1cac6d67bdc13dde1bf
L1nu5 <dr.abhi7deore@gmail.com>
2018-01-07 20:19:39 +0530

    Added date/time pickers (non-functional, values won't be saved)

--- Extension/js/overtime.js
+++ Extension/js/overtime.js
@@ -33,12 +33,12 @@ function generateTable() {
 		for (var i = 0; i < allKeys.length; ++i) {
 			var row = document.createElement('tr');
 			var currentKey = allKeys[i];
-			
+
 			storage.get([currentKey], function (result) {
 
 				var value = result[currentKey];
 				row.appendChild(document.createTextNode(currentKey));
-				for (var j = 0; j < 2; ++j){
+				for (var j = 0; j < 2; ++j) {
 					var td = document.createElement('td');
 					td.appendChild(document.createTextNode(value[storeKeys[j]]));
 					row.appendChild(td);
@@ -111,4 +111,25 @@ $("#pdf").click(function () {
 $("#about").click(function () {
 	$('html,body').animate({ scrollTop: $("#divAbout").offset().top },
 		'slow');
+});
+
+$('.datepicker').pickadate({
+	selectMonths: true, // Creates a dropdown to control month
+	selectYears: 15, // Creates a dropdown of 15 years to control year,
+	today: 'Today',
+	clear: 'Clear',
+	close: 'Ok',
+	closeOnSelect: false // Close upon selecting a date,
+});
+
+$('.timepicker').pickatime({
+	default: 'now', // Set default time: 'now', '1:30AM', '16:30'
+	fromnow: 0,       // set default time to * milliseconds from now (using with default = 'now')
+	twelvehour: false, // Use AM/PM or 24-hour formatss
+	donetext: 'OK', // text for done-button
+	cleartext: 'Clear', // text for clear-button
+	canceltext: 'Cancel', // Text for cancel-button
+	autoclose: false, // automatic close timepicker
+	ampmclickable: true, // make AM PM clickable
+	aftershow: function () { } //Function for after opening timepicker
 });
